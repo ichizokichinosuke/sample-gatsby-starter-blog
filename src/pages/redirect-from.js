3aebaecb693d26912fd4b64dba2c57e957cb67cd
ichizokichinosuke <nuuuukemw@gmail.com>
2022-07-08 20:20:26 +0900

    ckpt 0708

---
+++ src/pages/redirect-from.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+import Layout from "../components/layout"
+
+const RedirectFrom = () => {
+    return (
+        <Layout title="Redirect From">
+            <h1>See you!!</h1>
+        </Layout>
+    )
+}
+
+export default RedirectFrom
